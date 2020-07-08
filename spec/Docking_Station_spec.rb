8c33281d545f7b25bb3d5d65afbdd3933460d851
Student <student@Makerss-Air.broadband>
2020-07-08 17:00:09 +0100

    changed tests and methods to raise an error if there are no bikes available

--- spec/Docking_Station_spec.rb
+++ spec/Docking_Station_spec.rb
@@ -5,16 +5,21 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
   it 'releases a working bike' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
   end
   it 'docks a bike' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(subject.dock_bike(bike)).to eq(bike)
   end
   it 'checks a bike is availble' do
     bike = Bike.new
     subject.dock_bike(bike)
     expect(subject.bike).to eq(bike)
+  end
+  describe '#release_bike' do
+    it 'raises an error if no bike is available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 end
